1e39ef5b8dd3aa37320fdb4e7a6d1868c515606f
admin <admin@SML14>
2023-02-20 16:58:27 +0700

    Add project files.

---
+++ RazorPizzeria/Pages/PizzasController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RazorPizzeria.Data;
+using RazorPizzeria.Model;
+
+namespace RazorPizzeria.Pages
+{
+	public class PizzasController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+
+		public PizzasController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+		public IActionResult Index()
+		{
+			IEnumerable<PizzaOrder> objPizzaList = _db.PizzaOrders;
+			return View(objPizzaList);
+		}
+	}
+}
